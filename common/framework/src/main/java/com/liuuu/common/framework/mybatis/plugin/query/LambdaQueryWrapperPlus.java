65b72f38ca388aad198db409d8a54d0980b8ee14
liuuu <liugongmingg@163.com>
2024-07-24 11:18:08 +0800

    mybatis wrappers query

---
+++ common/framework/src/main/java/com/liuuu/common/framework/mybatis/plugin/query/LambdaQueryWrapperPlus.java
@@ -0,0 +1,136 @@
+package com.liuuu.common.framework.mybatis.plugin.query;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.support.SFunction;
+import com.liuuu.common.core.util.string.StrUtils;
+import org.springframework.util.CollectionUtils;
+
+import java.util.Collection;
+
+/**
+ * 扩展LambdaQueryWrapper类
+ *
+ * @Author Liuuu
+ * @Date 2024/7/23
+ */
+public class LambdaQueryWrapperPlus<T> extends LambdaQueryWrapper<T> {
+    /**
+     * 模糊查询
+     * @param column
+     * @param value
+     * @return
+     */
+    public LambdaQueryWrapperPlus<T> likeIf(SFunction<T, ?> column, String value) {
+        return StrUtils.isBlank(value) ? this : like(column, value);
+    }
+
+    @Override
+    public LambdaQueryWrapperPlus<T> like(SFunction<T, ?> column, Object value) {
+        return (LambdaQueryWrapperPlus<T>) super.like(column, value);
+    }
+
+    /**
+     * 相等
+     * @param column
+     * @param value
+     * @return
+     */
+    public LambdaQueryWrapperPlus<T> eqIf(SFunction<T, ?> column, Object value) {
+        return StrUtils.isNullBlank(value) ? this : eq(column, value);
+    }
+
+    @Override
+    public LambdaQueryWrapperPlus<T> eq(SFunction<T, ?> column, Object value) {
+        return (LambdaQueryWrapperPlus<T>) super.eq(column, value);
+    }
+
+    /**
+     * 包含
+     * @param column
+     * @param values
+     * @return
+     */
+    public LambdaQueryWrapperPlus<T> inIf(SFunction<T, ?> column, Collection<?> values) {
+        return CollectionUtils.isEmpty(values) ? this : in(column, values);
+    }
+
+    @Override
+    public LambdaQueryWrapperPlus<T> in(SFunction<T, ?> column, Collection<?> values) {
+        return (LambdaQueryWrapperPlus<T>) super.in(column, values);
+    }
+
+    /**
+     * 不包含
+     * @param column
+     * @param values
+     * @return
+     */
+    public LambdaQueryWrapperPlus<T> notInIf(SFunction<T, ?> column, Collection<?> values) {
+        return CollectionUtils.isEmpty(values) ? this : notIn(column, values);
+    }
+
+    @Override
+    public LambdaQueryWrapperPlus<T> notIn(SFunction<T, ?> column, Collection<?> values) {
+        return (LambdaQueryWrapperPlus<T>) super.notIn(column, values);
+    }
+
+    /**
+     * 小于
+     * @param column
+     * @param values
+     * @return
+     */
+    public LambdaQueryWrapperPlus<T> ltIf(SFunction<T, ?> column, Object value) {
+        return StrUtils.isNullBlank(value) ? this : lt(column, value);
+    }
+
+    @Override
+    public LambdaQueryWrapperPlus<T> lt(SFunction<T, ?> column, Object value) {
+        return (LambdaQueryWrapperPlus) super.lt(column, value);
+    }
+
+    /**
+     * 小于等于
+     * @param column
+     * @param value
+     * @return
+     */
+    public LambdaQueryWrapperPlus<T> leIf(SFunction<T, ?> column, Object value) {
+        return StrUtils.isNullBlank(value) ? this : le(column, value);
+    }
+
+    @Override
+    public LambdaQueryWrapperPlus<T> le(SFunction<T, ?> column, Object value) {
+        return (LambdaQueryWrapperPlus<T>) super.le(column, value);
+    }
+
+    /**
+     * 大于
+     * @param column
+     * @param value
+     * @return
+     */
+    public LambdaQueryWrapperPlus<T> gtIf(SFunction<T, ?> column, Object value) {
+        return StrUtils.isNullBlank(value) ? this : gt(column, value);
+    }
+
+    @Override
+    public LambdaQueryWrapperPlus<T> gt(SFunction<T, ?> column, Object value) {
+        return (LambdaQueryWrapperPlus<T>) super.gt(column, value);
+    }
+
+    /**
+     * 大于等于
+     * @param column
+     * @param value
+     * @return
+     */
+    public LambdaQueryWrapperPlus<T> geIf(SFunction<T, ?> column, Object value) {
+        return StrUtils.isNullBlank(value) ? this : ge(column, value);
+    }
+
+    @Override
+    public LambdaQueryWrapperPlus<T> ge(SFunction<T, ?> column, Object value) {
+        return (LambdaQueryWrapperPlus<T>) super.ge(column, value);
+    }
+}
--- common/framework/src/main/java/com/liuuu/common/framework/mybatis/plugin/query/QueryWrapperPlus.java
+++ common/framework/src/main/java/com/liuuu/common/framework/mybatis/plugin/query/QueryWrapperPlus.java
@@ -1,14 +1,26 @@
 package com.liuuu.common.framework.mybatis.plugin.query;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.enums.SqlKeyword;
+import com.baomidou.mybatisplus.core.metadata.TableInfo;
+import com.baomidou.mybatisplus.core.metadata.TableInfoHelper;
+import com.baomidou.mybatisplus.core.toolkit.ReflectionKit;
+import com.baomidou.mybatisplus.core.toolkit.support.SFunction;
 import com.liuuu.common.core.exception.ServiceException;
 import com.liuuu.common.core.util.message.MessageUtils;
 import com.liuuu.common.core.util.string.StrUtils;
 import com.liuuu.common.framework.mybatis.plugin.annotation.Query;
+import com.liuuu.common.framework.mybatis.plugin.constant.FieldConstant;
+import com.liuuu.common.framework.mybatis.plugin.enums.QueryWay;
+import com.sun.tools.javac.tree.JCTree;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.util.ClassUtils;
 
 import java.lang.reflect.Field;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -21,9 +33,61 @@ public class QueryWrapperPlus<T> extends QueryWrapper<T> {
     private static final Logger log = LoggerFactory.getLogger(QueryWrapperPlus.class);
 
     /**
-     * 排序字段分割表示
+     * 排序字段分割标识
      */
     private static final String ORDER_BY_COLUMN_SPLIT = ",";
+
+    /**
+     * 构建Wrapper查询条件
+     * @param clazz  PO实体类
+     * @param d  DTO实体参数对象
+     * @param selectColumns  查询返回的列
+     * @return
+     * @param <D>
+     */
+    public <D> QueryWrapperPlus<T> buildQueryWrapper(Class<T> clazz, D d, SFunction<T, ?>... selectColumns) {
+        QueryWrapperPlus queryWrapperPlus = new QueryWrapperPlus();
+
+        queryWrapperPlus.lambda().select(selectColumns);
+        if (d == null) {
+            return queryWrapperPlus;
+        }
+        // 获取T属性和列名
+        TableInfo tableInfo = TableInfoHelper.getTableInfo(clazz);
+        if (tableInfo == null) {
+            log.error("没有找到数据表对应的实体类, 当前传入的Clazz: {}", clazz);
+            throw new ServiceException(MessageUtils.message("system.exception"));
+        }
+        // T属性和列名的对应关系
+        Map<String, String> propertyColumnMap = new HashMap<>(tableInfo.getFieldList().size());
+        tableInfo.getFieldList().forEach(table -> {
+            propertyColumnMap.put(table.getProperty(), table.getColumn());
+        });
+        // 若主键不在fieldList中则单独获取
+        if (StrUtils.isNotBlank(tableInfo.getKeyProperty())) {
+            propertyColumnMap.put(tableInfo.getKeyProperty(), tableInfo.getKeyColumn());
+        }
+        // 获取d属性
+        List<Field> fieldList = ReflectionKit.getFieldList(ClassUtils.getUserClass(d.getClass()));
+        for (Field field : fieldList) {
+            field.setAccessible(true);
+            Query query = field.getAnnotation(Query.class);
+
+            String fieldName = getQueryFieldName(query, field);
+            if (query == null || query.ignore()) {
+                continue;
+            }
+            verifyQueryFieldNameExist(fieldName, propertyColumnMap);
+            Object value = getFieldValue(field, d);
+            if (StrUtils.isNullBlank(value) && (query == null || !query.empty())) {
+                continue;
+            }
+            String column = propertyColumnMap.get(fieldName);
+            buildQueryCondition(queryWrapperPlus, query, column, value);
+        }
+        buildOrderByCondition(queryWrapperPlus, propertyColumnMap, fieldList, d);
+        return queryWrapperPlus;
+    }
 
     /**
      * 获取查询属性名称
@@ -70,12 +134,107 @@ public class QueryWrapperPlus<T> extends QueryWrapper<T> {
 
     /**
      * 构建查询条件
-     * @param queryWrapperPlus
-     * @param query
-     * @param column
-     * @param value
+     * @param queryWrapperPlus  查询构造器
+     * @param query  查询注释
+     * @param column  列名称
+     * @param value  值
      */
     private void buildQueryCondition(QueryWrapperPlus queryWrapperPlus, Query query, String column, Object value) {
+        if (query == null || query.value() == null) {
+            queryWrapperPlus.eq(column, value);
+            return;
+        }
+        QueryWay queryWay = query.value();
+        switch (queryWay) {
+            case EQ:
+                queryWrapperPlus.eq(column, value);
+                break;
+            case NE:
+                queryWrapperPlus.ne(column, value);
+                break;
+            case GT:
+                queryWrapperPlus.gt(column, value);
+                break;
+            case GE:
+                queryWrapperPlus.ge(column, value);
+                break;
+            case LT:
+                queryWrapperPlus.lt(column, value);
+                break;
+            case LE:
+                queryWrapperPlus.le(column, value);
+                break;
+            case LIKE:
+                queryWrapperPlus.like(column, value);
+                break;
+            case NOT_LIKE:
+                queryWrapperPlus.notLike(column, value);
+                break;
+            case LIKE_LEFT:
+                queryWrapperPlus.likeLeft(column, value);
+                break;
+            case LIKE_RIGHT:
+                queryWrapperPlus.likeRight(column, value);
+                break;
+            case IN:
+                if (value instanceof Collection) {
+                    Collection coll = (Collection) value;
+                    queryWrapperPlus.in(column, coll);
+                } else {
+                    queryWrapperPlus.in(column, value);
+                }
+                break;
+            case NOT_IN:
+                if (value instanceof Collection) {
+                    Collection coll = (Collection) value;
+                    queryWrapperPlus.notIn(column, coll);
+                } else {
+                    queryWrapperPlus.notIn(column, value);
+                }
+                break;
+            default:
+                queryWrapperPlus.eq(column, value);
+        }
+    }
+
+    /**
+     * 构造排序
+     * @param queryWrapperPlus  查询构造器
+     * @param propertyColumnMap  属性和列名
+     * @param fieldList  属性列表
+     * @param d  参数
+     * @param <D>
+     */
+    private <D> void buildOrderByCondition(QueryWrapperPlus queryWrapperPlus, Map<String, String> propertyColumnMap,
+                                           List<Field> fieldList, D d) {
+        // 排序列
+        Field orderColumnField = fieldList.stream().filter(f -> FieldConstant.ORDER_COLUMN.equals(f.getName())).findFirst().orElse(null);
+        Object orderColumnObject = getFieldValue(orderColumnField, d);
+        if (!(orderColumnObject instanceof String) || StrUtils.isNullBlank(orderColumnObject)) {
+            return;
+        }
+
+        // 排序类型
+        Field orderTypeField = fieldList.stream().filter(f -> FieldConstant.ORDER_TYPE.equals(f.getName())).findFirst().orElse(null);
+        Object orderTypeObject = getFieldValue(orderTypeField, d);
+
+        String[] orderColumns = orderColumnObject.toString().split(ORDER_BY_COLUMN_SPLIT);
+        String[] orderTypes = orderTypeObject == null ? new String[] {} : orderTypeObject.toString().split(ORDER_BY_COLUMN_SPLIT);
+
+        // 构造排序
+        for (int i = 0; i < orderColumns.length; i++) {
+            String orderColumn = orderColumns[i];
+            if (StrUtils.isBlank(orderColumn)) {
+                continue;
+            }
+            String column =propertyColumnMap.get(orderColumn);
+            if (StrUtils.isBlank(column)) {
+                String message = StrUtils.format("排序字段: {}, 不存在", orderColumn);
+                throw new ServiceException(message);
+            }
+            boolean isAsc = i < orderTypes.length ? SqlKeyword.ASC.getSqlSegment().equalsIgnoreCase(orderTypes[i]) : true;
+            queryWrapperPlus.orderBy(true, isAsc, column);
+        }
 
     }
 }
